42f6afd82edfbde4c9d3e89b9aa099c40563c1b8
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-08-03 01:57:09 -0300

    Projeto iniciado

---
+++ src/test/java/br/com/gestao/gestaovotosms/GestaoVotosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestao.gestaovotosms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVotosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
